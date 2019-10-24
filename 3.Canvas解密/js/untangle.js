3dd6fe13723b9c01355d4774b1c92e1df951dbfa
bluebean <1401597377@qq.com>
2019-10-24 23:47:01 +0800

    new

--- 3.Canvas解密/js/untangle.js
+++ 3.Canvas解密/js/untangle.js
@@ -10,9 +10,57 @@ function Line(startPoint, endPoint,thickness){
 	this.thickness = thickness;
 }
 
+function isIntersect(line1,line2){
+	//
+	var a1 = line1.endPoint.y - line1.startPoint.y;
+	var b1 = line1.startPoint.x - line1.endPoint.x;
+	var c1 = a1*line1.startPoint.x + b1*line1.startPoint.y;
+	//
+	var a2 = line2.endPoint.y - line2.startPoint.y;
+	var b2 = line2.startPoint.x - line2.endPoint.x;
+	var c2 = a2*line2.startPoint.x + b2*line2.startPoint.y;
+	
+	var d = a1*b2 - a2*b1;
+	
+	if(d==0){
+		return false;
+	}else{
+		var x = (b2*c1 - b1*c2)/d;
+		var y = (a1*c2 - a2*c1)/d;
+		if((isInBetween(line1.startPoint.x, x, line1.endPoint.x) || isInBetween(line1.startPoint.y,y,line1.endPoint.y))
+			&& 
+		   (isInBetween(line2.startPoint.x, x, line2.endPoint.x) || isInBetween(line2.startPoint.y,y,line2.endPoint.y))
+		){
+			return true;
+		}
+	}
+	return false;
+}
+
+function isInBetween(a,b,c){
+	if(Math.abs(a-b)<0.00001 || Math.abs(b-c)<0.00001){
+		return false;
+	}
+	return (a<b && b<c) || (c<b && b<a);
+}
+
+function updateLineIntersection(){
+	for(var i=0;i<untangleGame.lines.length;i++){
+		for(var j=0;j<i;j++){
+			var line1 = untangleGame.lines[i];
+			var line2 = untangleGame.lines[j];
+			if(isIntersect(line1,line2)){
+				line1.thickness = untangleGame.boldLineThickness;
+				line2.thickness = untangleGame.boldLineThickness;
+			}
+		}
+	}
+}
+
 var untangleGame = {
 	circles:[],
 	thinLineThickness:1,
+	boldLineThickness:5,
 	lines:[],
 };
 
@@ -42,6 +90,7 @@ function connectCircles(ctx){
 			untangleGame.lines.push(new Line(startPoint,endPoint,untangleGame.thinLineThickness));
 		}
 	}
+	updateLineIntersection();
 }
 
 function clear(ctx, canvas){
